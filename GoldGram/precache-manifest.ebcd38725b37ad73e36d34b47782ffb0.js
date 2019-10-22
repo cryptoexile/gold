dfea7dbbcf39e534d664916d1be185fd44bb3b65
RN <cryptoexile@protonmail.com>
2019-10-22 09:02:56 +0530

    Uploading Partial Gold Site for Testing

---
+++ GoldGram/precache-manifest.ebcd38725b37ad73e36d34b47782ffb0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019204cc3e8fd1e8b22975485bf3868d",
+    "url": "/GoldGram/4b919fbd60e8eb8f3e5f.worker.js"
+  },
+  {
+    "revision": "9b19fda43f719086572c1c3fa1c44a60",
+    "url": "/GoldGram/b6908399039bbaf25b77.worker.js"
+  },
+  {
+    "revision": "105304e3628de5e2d47ba8e644d905f8",
+    "url": "/GoldGram/index.html"
+  },
+  {
+    "revision": "0a43ff2ef72253e6e7ea",
+    "url": "/GoldGram/static/css/4.1fb39402.chunk.css"
+  },
+  {
+    "revision": "c99c8277cc471aba6b02",
+    "url": "/GoldGram/static/css/5.1b8f2a33.chunk.css"
+  },
+  {
+    "revision": "910150e36a2eee47b390",
+    "url": "/GoldGram/static/css/main.f09dd451.chunk.css"
+  },
+  {
+    "revision": "d362fb0efe7ce5350e02",
+    "url": "/GoldGram/static/js/2.29ff5147.chunk.js"
+  },
+  {
+    "revision": "f7b89039d8d8c5e3518e",
+    "url": "/GoldGram/static/js/3.c2f767a8.chunk.js"
+  },
+  {
+    "revision": "0a43ff2ef72253e6e7ea",
+    "url": "/GoldGram/static/js/4.8351e557.chunk.js"
+  },
+  {
+    "revision": "c99c8277cc471aba6b02",
+    "url": "/GoldGram/static/js/5.ad11464e.chunk.js"
+  },
+  {
+    "revision": "de8a9a0ccdcadd0a80a5",
+    "url": "/GoldGram/static/js/6.a00a9b61.chunk.js"
+  },
+  {
+    "revision": "910150e36a2eee47b390",
+    "url": "/GoldGram/static/js/main.320a8cb9.chunk.js"
+  },
+  {
+    "revision": "a4cde300764a6d982a08",
+    "url": "/GoldGram/static/js/runtime~main.ecffcbea.js"
+  },
+  {
+    "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
+    "url": "/GoldGram/static/media/Bubbles.5b7ac6f8.svg"
+  },
+  {
+    "revision": "b942e5ab30e2453deffb5c5a8f5b1264",
+    "url": "/GoldGram/static/media/General_2x.b942e5ab.png"
+  },
+  {
+    "revision": "e4c5bd76f3f6810c07a40e55b38c3054",
+    "url": "/GoldGram/static/media/Manytabs_2x.e4c5bd76.png"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/GoldGram/static/media/check.71da7469.svg"
+  }
+]);
